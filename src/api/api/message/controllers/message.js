2d5227825f4c7f8965ee5f86f75c2796b10e668e
suraj <surajsharma@pm.me>
2020-07-06 17:47:35 +0530

    strapi api without custom routes

---
+++ src/api/api/message/controllers/message.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  index: async (ctx) => {
+    ctx.send("Hello World!");
+  },
+};

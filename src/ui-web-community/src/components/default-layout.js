4cd46afd9262c60a1339d10f79c63302c8c9dc79
denny <denny.george90@gmail.com>
2020-07-13 00:37:13 +0530

    (feat) change styling for default layout. The app's layout component now use the updated UI component from the new bit registry

--- src/ui-web-community/src/components/default-layout.js
+++ src/ui-web-community/src/components/default-layout.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import { primaryNav, footerItems } from '../config/options';
-import AppShell from '../components/atomic/AppShell';
+import React from "react"
+import { primaryNav, footerItems } from "../config/options"
+import { AppShell } from "@bit/tattle-tech.core-ui.app-shell"
 /**
-* @author
-* @function DefaultLayout
-**/
+ * @author
+ * @function DefaultLayout
+ **/
 
-const DefaultLayout = ({children}) => (
-    <AppShell
-        headerLabel={'Tattle'}
-        footerItems={footerItems}
-        primaryNav={primaryNav}
-        expandCenter={true}
-    >
-        {children}
-    </AppShell>
+const DefaultLayout = ({ children }) => (
+  <AppShell
+    headerLabel={"Whatsapp Scraper"}
+    headerTarget={"/"}
+    footerItems={footerItems}
+    primaryNav={primaryNav}
+    expandCenter={true}
+  >
+    {children}
+  </AppShell>
 )
 
 export default DefaultLayout
